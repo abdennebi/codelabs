222b07a91c885d7f7f322cf08f04a3aa91910d1e
Mohamed Abdennebi <abdennebi.dev@gmail.com>
2023-04-29 11:21:25 +0200

    added apis

--- google-cloud/psc-consumer/main.tf
+++ google-cloud/psc-consumer/main.tf
@@ -11,3 +11,22 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+# https://cloud.google.com/service-directory/docs/reference/rest
+resource "google_project_service" "servicedirectory" {
+  service = "servicedirectory.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "dns" {
+  service = "dns.googleapis.com "
+  disable_on_destroy = false
+}
+
+
+
--- google-cloud/psc-producer/network.tf
+++ google-cloud/psc-producer/network.tf
@@ -10,6 +10,9 @@ resource "google_compute_subnetwork" "producer_euw1" {
   network       = google_compute_network.vpc_producer.id
 }
 
+# Packets from the consumer VPC network are translated using source NAT (SNAT) so that their original source IP addresses
+# are converted to source IP addresses from the NAT subnet in the producer's VPC network.
+# See: https://cloud.google.com/vpc/docs/about-vpc-hosted-services#psc-subnets
 resource "google_compute_subnetwork" "producer_nat" {
   name          = "producer-nat"
   ip_cidr_range = "192.168.0.0/24"
